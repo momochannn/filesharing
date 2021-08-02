3d456569b4b166f0211662835de9a91432aa5096
momochannn <84643296+momochannn@users.noreply.github.com>
2021-08-02 17:44:27 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,59 +1,42 @@
 
-from pyrogram import Client, filters
+import asyncio
+from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait
+
 from bot import Bot
-from config import ADMINS
-from helper_func import encode, get_message_id
+from config import ADMINS, CHANNEL_ID
+from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
-async def batch(client: Client, message: Message):
-    while True:
-        try:
-            first_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
-        except:
-            return
-        f_msg_id = await get_message_id(client, first_message)
-        if f_msg_id:
-            break
-        else:
-            await first_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
-            continue
-
-    while True:
-        try:
-            second_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
-        except:
-            return
-        s_msg_id = await get_message_id(client, second_message)
-        if s_msg_id:
-            break
-        else:
-            await second_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
-            continue
-
-
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','batch','genlink']))
+async def channel_post(client: Client, message: Message):
+    reply_text = await message.reply_text("Tolong tunggu...!", quote = True)
+    try:
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Sepertinya ada yang salah..!")
+        return
+    converted_id = post_message.message_id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>INI LINK ANDA</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
+    await reply_text.edit("SILAHKAN CEK TOMBOL DI BAWAH", reply_markup=reply_markup, disable_web_page_preview = True)
+    await post_message.edit_reply_markup(reply_markup)
 
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
-async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
-        else:
-            await channel_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
-            continue
-
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited)
+async def new_post(client: Client, message: Message):
+    converted_id = message.message_id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>INI LINK ANDA</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
+    try:
+        await message.edit_reply_markup(reply_markup)
+    except Exception as e:
+        print(e)
+        pass
