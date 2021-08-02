8cd0c85242f6bfbf00e19a63bce63e72cf2b4b38
momochannn <84643296+momochannn@users.noreply.github.com>
2021-08-02 17:44:07 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,105 +1,59 @@
 
-import asyncio
-from pyrogram import Client, filters, __version__
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import FloodWait
-
 from bot import Bot
-from config import ADMINS, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
-from helper_func import subscribed, encode, decode, get_messages
+from config import ADMINS
+from helper_func import encode, get_message_id
 
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
-async def start_command(client: Client, message: Message):
-    text = message.text
-    if len(text)>7:
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+async def batch(client: Client, message: Message):
+    while True:
         try:
-            base64_string = text.split(" ", 1)[1]
+            first_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
         except:
             return
-        string = await decode(base64_string)
-        argument = string.split("-")
-        if len(argument) == 3:
-            try:
-                start = int(int(argument[1]) / abs(client.db_channel.id))
-                end = int(int(argument[2]) / abs(client.db_channel.id))
-            except:
-                return
-            if start <= end:
-                ids = range(start,end+1)
-            else:
-                ids = []
-                i = start
-                while True:
-                    ids.append(i)
-                    i -= 1
-                    if i < end:
-                        break
-        elif len(argument) == 2:
-            try:
-                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
-            except:
-                return
-        temp_msg = await message.reply("Harap tunggu ...")
+        f_msg_id = await get_message_id(client, first_message)
+        if f_msg_id:
+            break
+        else:
+            await first_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
+            continue
+
+    while True:
         try:
-            messages = await get_messages(client, ids)
+            second_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
         except:
-            await message.reply_text("Ada yang tidak beres ..!")
             return
-        await temp_msg.delete()
-        for msg in messages:
+        s_msg_id = await get_message_id(client, second_message)
+        if s_msg_id:
+            break
+        else:
+            await second_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
+            continue
 
-            if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-            else:
-                caption = "" if not msg.caption else msg.caption.html
 
-            if DISABLE_CHANNEL_BUTTON:
-                reply_markup = msg.reply_markup
-            else:
-                reply_markup = None
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>INI LINK ANDA</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
-            try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
-                await asyncio.sleep(0.5)
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
-            except:
-                pass
-        return
-    else:         
-        reply_markup = InlineKeyboardMarkup(
-            [
-               [
-                InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink), 
-              ],[
-                InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                InlineKeyboardButton("⛔ TUTUP ⛔", callback_data = "close")
-               ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(firstname = message.chat.first_name),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
-        return
 
-@Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    text = "<b>Anda harus join channel/Group untuk menggunakan saya\n\nTolong bergabunglah ke Channel</b>"
-    message_text = message.text
-    try:
-        command, argument = message_text.split()
-        text = text + f" <b>Kalau belum join gak bisa buka file nya kalau sudah join silahkan klik refresh</b>"
-    except ValueError:
-        pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("💌 JOIN CHANNEL 💌", url = client.invitelink)],[InlineKeyboardButton("🔄 REFRESH", url = f"https://t.me/{client.username}?start={argument}")]])
-    await message.reply(
-        text = text,
-        reply_markup = reply_markup,
-        quote = True,
-        disable_web_page_preview = True
-    )
-    
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+async def link_generator(client: Client, message: Message):
+    while True:
+        try:
+            channel_message = await client.ask(text = "Teruskan Pesan dari Saluran DB (with Quotes) ..\natau Kirim tautan Posting Saluran DB", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
+        except:
+            return
+        msg_id = await get_message_id(client, channel_message)
+        if msg_id:
+            break
+        else:
+            await channel_message.reply("❌ KESALAHAN\n\nPos yang Diteruskan ini bukan dari Saluran DB saya atau Tautan ini tidak diambil dari Saluran DB", quote = True)
+            continue
+
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>INI LINK ANDA</b>\n\n{link}", quote=True, reply_markup=reply_markup)
