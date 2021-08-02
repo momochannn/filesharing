1b243de6ff710974a9e70000ff29477e05a766e1
momochannn <84643296+momochannn@users.noreply.github.com>
2021-08-02 18:02:13 +0800

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,4 +1,3 @@
-#(©)Codexbotz
 
 import asyncio
 from pyrogram import filters, Client
@@ -6,12 +5,12 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_ID
 from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','batch','genlink']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Harap tunggu...", quote = True)
+    reply_text = await message.reply_text("Tolong tunggu...!", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
@@ -19,31 +18,23 @@ async def channel_post(client: Client, message: Message):
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Terjadi kesalahan..!")
+        await reply_text.edit_text("Sepertinya ada yang salah..!")
         return
     converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"<b>Ini link kamu</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
-
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
+    await reply_text.edit("SILAHKAN CEK TOMBOL DI BAWAH", reply_markup=reply_markup, disable_web_page_preview = True)
+    await post_message.edit_reply_markup(reply_markup)
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited)
 async def new_post(client: Client, message: Message):
-
-    if DISABLE_CHANNEL_BUTTON:
-        return
-
     converted_id = message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇 SHARE", url=f'https://telegram.me/share/url?url={link}'), InlineKeyboardButton("📥 DOWNLOAD", url = f"{link}")]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
