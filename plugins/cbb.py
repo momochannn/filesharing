4c62fad9f3c3e1cc3982634cf36cf9b64cd0c1ef
momochannn <84643296+momochannn@users.noreply.github.com>
2021-08-02 17:25:03 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,18 +1,21 @@
+#(©)Codexbotz
+
 from pyrogram import __version__
 from bot import Bot
+from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from config import CHANNEL
+
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ CHANNEL ASUPAN : @{CHANNEL}</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Taniya Amelia</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @palingviral\n○ Group : @palingviralgrup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("❌ TUTUP", callback_data = "close")
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
             )
